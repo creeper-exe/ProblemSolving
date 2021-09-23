7fd04a0b66b1f7d56f70010c77263a5cec2d5c7a
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-09-23 23:00:08 +0300

    Create  Day1DataTypes.cpp

---
+++ HackerRank/30 Days of Code/Day 1: Data Types/Day1DataTypes.cpp
@@ -0,0 +1,7 @@
+    string ss, n, dd;
+    getline(cin, n);  
+    getline(cin, dd);    
+    getline(cin, ss); 
+    cout << int(stoi (n) + i) << endl;
+    cout << fixed << setprecision(1) << double(stod(dd) + d ) << endl;
+    cout << s << ss << endl;
