fe27a0fa8e31d0dd1b9f37a0ee2c9f5a5657c348
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-09-28 23:44:30 +0300

    Create Day6Let'sReview.cpp

---
+++ HackerRank/30 Days of Code/Day 6: Let's Review/Day6Let'sReview.cpp
@@ -0,0 +1,18 @@
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        
+        string even = "", odd = "";
+        for (int j = 0; j < s.length(); j++) {
+            if (j % 2 == 0)
+                even += s[j];
+            else
+                odd += s[j];
+        }
+        cout << even << " " << odd << endl;
+    }
+    
