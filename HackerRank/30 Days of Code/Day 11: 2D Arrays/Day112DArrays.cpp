d7148cdcc60b2fdcf34557e252102f75254cafdb
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-10-04 21:56:18 +0300

    Update Day112DArrays.cpp

--- HackerRank/30 Days of Code/Day 11: 2D Arrays/Day112DArrays.cpp
+++ HackerRank/30 Days of Code/Day 11: 2D Arrays/Day112DArrays.cpp
@@ -1,24 +1,4 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
+
 using namespace std;
 
 int add(int arr[6][6], int arr_i, int arr_j){
