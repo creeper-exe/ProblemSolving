7ad210cb925a9deb963d3c84398b050e020b4936
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-10-07 22:40:38 +0300

    Update Day14Scope.cpp

--- HackerRank/30 Days of Code/Day 14: Scope/Day14Scope.cpp
+++ HackerRank/30 Days of Code/Day 14: Scope/Day14Scope.cpp
@@ -1,1 +1,10 @@
 
+    Difference(vector<int> arr){
+        elements = arr;
+        sort(elements.begin(), elements.end());
+    }
+
+    void computeDifference(){
+        
+        maximumDifference = abs(elements[elements.size()-1] - elements[0]);
+    }
