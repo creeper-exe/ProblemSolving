8ae76b1865a8391da07d8a0bb25b5823bd0d5aec
Noureldin Ehab <57595625+creeper-exe@users.noreply.github.com>
2021-10-10 20:55:03 +0300

    Update Day17MoreExceptions.cpp

--- HackerRank/30 Days of Code/Day 17: More Exceptions/Day17MoreExceptions.cpp
+++ HackerRank/30 Days of Code/Day 17: More Exceptions/Day17MoreExceptions.cpp
@@ -1,1 +1,9 @@
 
+using namespace std;
+struct Calculator {
+    static int power(const int n_, const int p_) {
+        //Write your code here
+        if ( n_ < 0 || p_ < 0 ) { throw std::invalid_argument("n and p should be non-negative");}
+        return pow(n_, p_);
+    }
+};
